97685f0086b289d5bf6e3bca373624de6594d2a2
Dmytro Nosulich <dmytro.nosulich@gmail.com>
2026-01-18 22:20:32 +0100

    add initial project

---
+++ TimeTracker/TimeTracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TimeTracker
+//
+//  Created by Dmytro Nosulich on 18.01.26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ TimeTracker/TimeTrackerTests/TimeTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerTests.swift
+//  TimeTrackerTests
+//
+//  Created by Dmytro Nosulich on 18.01.26.
+//
+
+import Testing
+@testable import TimeTracker
+
+struct TimeTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
