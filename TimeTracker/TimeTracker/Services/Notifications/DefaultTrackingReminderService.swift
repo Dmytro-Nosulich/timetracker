e6e0776686d65fa19749e90478ab8b38642cb346
Dmytro Nosulich <dmytro.nosulich@gmail.com>
2026-06-24 21:29:37 +0200

    [fix] don't show notification reminder when timer is on

--- TimeTracker/TimeTracker/Services/Notifications/DefaultTrackingReminderService.swift
+++ TimeTracker/TimeTracker/Services/Notifications/DefaultTrackingReminderService.swift
@@ -2,11 +2,12 @@ import Foundation
 import UserNotifications
 
 @MainActor
-final class DefaultTrackingReminderService: TrackingReminderService {
+final class DefaultTrackingReminderService: NSObject, TrackingReminderService {
     private let userPreferences: UserPreferencesService
     private let timerService: TimerService
     private let notificationCenter: UNUserNotificationCenter
 
+	nonisolated
     private static let notificationCategoryId = "trackingReminder"
     private static let notificationIdPrefix = "tracking-reminder-day-"
 
@@ -18,6 +19,8 @@ final class DefaultTrackingReminderService: TrackingReminderService {
         self.userPreferences = userPreferences
         self.timerService = timerService
         self.notificationCenter = notificationCenter
+        super.init()
+        notificationCenter.delegate = self
     }
 
     func rescheduleNotifications() {
@@ -57,5 +60,23 @@ final class DefaultTrackingReminderService: TrackingReminderService {
 
     func requestPermissionIfNeeded() {
         notificationCenter.requestAuthorization(options: [.alert, .sound]) { _, _ in }
+    }
+}
+
+extension DefaultTrackingReminderService: UNUserNotificationCenterDelegate {
+    nonisolated func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        guard notification.request.content.categoryIdentifier == Self.notificationCategoryId else {
+            completionHandler([.banner, .sound])
+            return
+        }
+
+        let options: UNNotificationPresentationOptions = MainActor.assumeIsolated {
+            timerService.state == .running ? [] : [.banner, .sound]
+        }
+        completionHandler(options)
     }
 }
